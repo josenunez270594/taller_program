fa3e7b7d2909c9226eea0c5ee863238aee4ca66b
josenunez270594 <158522198+josenunez270594@users.noreply.github.com>
2024-03-01 10:30:16 -0500

    work1.py
    he corregido el contador

--- work1.py
+++ work1.py
@@ -3,19 +3,18 @@
 #20. 
 datos = int(input('cantidad de numeros que desea ingresar:'))
 import os
-contador = 0
+contador = 1
 lista_num =[]
 lista_num2 =[]
 os.system('cls')
-while (contador < datos):
+while (contador <= datos):
     os.system('cls')
     num = int(input('escriba un numero'))
     if num>=10 and num <= 20:
         lista_num.append(num)
-        contador += 1
     else:
         lista_num2.append(num)
-        contador += 1               
+    contador += 1               
     print(f'estos numeros estan dentro del rango: {lista_num}\n estos fueron los numeros que no estan en el rango: {lista_num2}')
-    
+    # no es necesario imprimir los que no estan en el rango pero lo hice para corroborar que almacena los que almacena correctamente
         
